68442baab087daec544577ae011c02e9151585db
juang0nz <101676463+juang0nz@users.noreply.github.com>
2022-11-14 18:32:58 -0300

    Trabajo terminado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,7 +3,7 @@ using Library;
 
 namespace Program
 {
-    class Program<T>
+    class Program
     {
         static void Main(string[] args)
         {
@@ -17,40 +17,44 @@ namespace Program
             Person p8 = new Person ("Amadeo",20);
 
 
-            Node<Person> np1 = new Node <Person> (1);
-            Node<Person> np2 = new Node <Person> (2);
-            Node<Person> np3 = new Node <Person> (3);
-            Node<Person> np4 = new Node <Person> (4);
-            Node<Person> np5 = new Node <Person> (5);
-            Node<Person> np6 = new Node <Person> (6);
-            Node<Person> np7 = new Node <Person> (7);
-            Node<Person> np8 = new Node <Person> (8);
+            Node<Person> np1 = new Node <Person> (p1);
+            Node<Person> np2 = new Node <Person> (p2);
+            Node<Person> np3 = new Node <Person> (p3);
+            Node<Person> np4 = new Node <Person> (p4);
+            Node<Person> np5 = new Node <Person> (p5);
+            Node<Person> np6 = new Node <Person> (p6);
+            Node<Person> np7 = new Node <Person> (p7);
+            Node<Person> np8 = new Node <Person> (p8);
 
             //en el nodo de Maria agrego estos dos:
         
-            np4.AddChildren(p2); //agrego a pedro como hijo de Maria
-            np4.AddChildren(p3); //agrego Agustin como hijo de Maria
+            np4.AddChildren(np2); //agrego a pedro como hijo de Maria
+            np4.AddChildren(np3); //agrego Agustin como hijo de Maria
 
             //el segundo:
 
-            np2.AddChildren(p1); //agrego a Juan como hijo de Pedro
-            np2.AddChildren(p8); //agrego a Amadeo como hijo de Pedro
-            np3.AddChildren(p6); //agrego a Facundo como hijo de Agustin
+            np2.AddChildren(np1); //agrego a Juan como hijo de Pedro
+            np2.AddChildren(np8); //agrego a Amadeo como hijo de Pedro
+            np3.AddChildren(np6); //agrego a Facundo como hijo de Agustin
 
             //el tercero:
 
-            np6.AddChildren(p5); //agrego a Laura como hija de Facundo
+            np6.AddChildren(np5); //agrego a Laura como hija de Facundo
 
+    
+            SumVisitor v1 = new SumVisitor();
+            VisitorName v2 = new VisitorName();
+            VisitorOld v3 = new VisitorOld();
+            v1.Visit (np4);
+         
 
-           /* Node<T> n1 = new Node<T>(1);
-            Node<T> n2 = new Node<T>(2);
-            Node<T> n3 = new Node<T>(3);
-            Node<T> n4 = new Node<T>(4);
-            Node<T> n5 = new Node<T>(5);
-            Node<T> n6 = new Node<T>(6);
-            Node<T> n7 = new Node<T>(7);*/
+            np1.Accept(v1);
+            Console.WriteLine($"La suma de las edades es:{v1.SumaEdades}");
+            np1.Accept(v2);
+            Console.WriteLine($"La person con el nombre mas largo es {v2.LongestName}");
+            np1.Accept(v3);
+            Console.WriteLine($"El mayor de todos es {v3.OldestKid}");
 
-            // visitar el árbol aquí
         }
     }
 }
